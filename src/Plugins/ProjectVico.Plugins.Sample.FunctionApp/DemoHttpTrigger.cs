0184e3ebb27d469eabe87373aa76c61474f25b60
Henning Kilset <hekilset@microsoft.com>
2023-08-21 17:02:14 +0200

    Added OpenAPI packages to the Sample Plugin project

--- src/Plugins/ProjectVico.Plugins.Sample.FunctionApp/DemoHttpTrigger.cs
+++ src/Plugins/ProjectVico.Plugins.Sample.FunctionApp/DemoHttpTrigger.cs
@@ -1,7 +1,9 @@
 ﻿using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ProjectVico.Plugins.Sample.FunctionApp;
 
@@ -11,18 +13,22 @@ public class DemoHttpTrigger
 
     public DemoHttpTrigger(ILoggerFactory loggerFactory)
     {
-        _logger = loggerFactory.CreateLogger<DemoHttpTrigger>();
+        //_logger = loggerFactory.CreateLogger<DemoHttpTrigger>();
     }
 
     [Function("DemoHttpTrigger")]
+    [OpenApiOperation(operationId: "Query", tags: new[] { "ExecuteFunction" }, Description = "Returns the same string back as passed in")]
+    [OpenApiParameter(name: "number1", Description = "The first number to add", Required = true, In = ParameterLocation.Query)]
+    [OpenApiParameter(name: "number2", Description = "The second number to add", Required = true, In = ParameterLocation.Query)]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "Returns the sum of the two numbers.")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/json", bodyType: typeof(string), Description = "Returns the error of the input.")]
     public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        //_logger.LogInformation("C# HTTP trigger function processed a request.");
 
         var response = req.CreateResponse(HttpStatusCode.OK);
         response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-        response.WriteString("Welcome to Azure Functions!");
+        response.WriteString(req.Body.ToString() ?? string.Empty);
 
         return response;
     }
