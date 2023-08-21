92649cd27a9f1ab09d9ef120a70c5e3ee68115f5
Henning Kilset <hekilset@microsoft.com>
2023-08-21 15:37:01 +0200

    Added sample FunctionApp plugins project - no changes from default templates for now

---
+++ src/Plugins/ProjectVico.Plugins.Sample.FunctionApp/DemoHttpTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectVico.Plugins.Sample.FunctionApp;
+
+public class DemoHttpTrigger
+{
+    private readonly ILogger _logger;
+
+    public DemoHttpTrigger(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<DemoHttpTrigger>();
+    }
+
+    [Function("DemoHttpTrigger")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
+    }
+}
