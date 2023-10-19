265173d44df32920dcb9aa11596813467d5b7ee5
Henning Kilset <hekilset@microsoft.com>
2023-10-19 16:06:11 +0200

    Merge test (#107)
    * Modified gitignore to handle local chat session storage
    
    * Work in progress on #20 - adding plugins through code enumeration instead of manually.
    
    * Added Microsoft Word plugin project - only default template for now
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete Sprint1/LICENSE-HACK23/data/env_reports_and_eis/env_reports/lwr/exelon_clinton/Grace's Garden Article and Photos.pdf
    
    * Delete Sprint1/LICENSE-HACK23/data/env_reports_and_eis/env_reports/lwr/exelon_clinton/Grace's Garden.pdf
    
    * Add files via upload
    
    * Create build GH Actions for copilot frontend and backend (#57)
    
    * Create plugin-build-DocQnA.yml (#58)
    
    * Update copilot-build-backend.yml (#59)
    
    * Create plugin-build-GeographicalData.yml (#60)
    
    * Removed temporary MS word project
    
    * Removed old word addin code
    
    * adding wordclient (#66)
    
    ---------
    
    Co-authored-by: ninarutzen <143031612+ninarutzen@users.noreply.github.com>
    Co-authored-by: sebasrivera96 <sebas.rivera96@gmail.com>
    Co-authored-by: Genady Belenky <26817885+gbelenky@users.noreply.github.com>

---
+++ src/Frontend/Copilot/word-blazor-add-in/Models/Chat.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorAddIn.Models
+{
+    
+    public enum Direction
+    {
+        To,
+        From
+    }
+    public class Message
+    {
+        public string? Text { get; set; } = string.Empty;
+        public Direction MessageDirection { get; set; } = Direction.From;
+    }
+}
