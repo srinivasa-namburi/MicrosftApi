cdb58079a352dc602e28b9784155caea72270655
Henning Kilset <hekilset@microsoft.com>
2024-04-10 02:21:49 +0200

    Fixed ServiceDefaults so Service Discovery works again with P5.

--- src/ProjectVico.V2.Web.DocGen/Program.cs
+++ src/ProjectVico.V2.Web.DocGen/Program.cs
@@ -32,28 +32,23 @@ builder.Services.Configure<ServiceConfigurationOptions>(serviceConfigurationSect
 
 builder.Services.AddHttpClient<IDocumentGenerationApiClient, DocumentGenerationApiClient>(httpClient =>
 {
-    //httpClient.BaseAddress = new("https://api-main");
-    httpClient.BaseAddress = new("https://localhost:6001");
+    httpClient.BaseAddress = new("https+http://api-main");
 });
 builder.Services.AddHttpClient<IDocumentIngestionApiClient, DocumentIngestionApiClient>(httpClient =>
 {
-    //httpClient.BaseAddress = new("https://api-main");
-    httpClient.BaseAddress = new("https://localhost:6001");
+    httpClient.BaseAddress = new("https+http://api-main");
 });
 builder.Services.AddHttpClient<IContentNodeApiClient, ContentNodeApiClient>(httpClient =>
 {
-    //httpClient.BaseAddress = new("https://api-main");
-    httpClient.BaseAddress = new("https://localhost:6001");
+    httpClient.BaseAddress = new("https+http://api-main");
 });
 builder.Services.AddHttpClient<IChatApiClient, ChatApiClient>(httpClient =>
 {
-    //httpClient.BaseAddress = new("https://api-main");
-    httpClient.BaseAddress = new("https://localhost:6001");
+    httpClient.BaseAddress = new("https+http://api-main");
 });
 builder.Services.AddHttpClient<IAuthorizationApiClient, AuthorizationApiClient>(httpClient =>
 {
-    //httpClient.BaseAddress = new("https://api-main");
-    httpClient.BaseAddress = new("https://localhost:6001");
+    httpClient.BaseAddress = new("https+http://api-main");
 });
 
 // Add services to the container.
@@ -143,15 +138,13 @@ builder.Services.AddAuthentication("MicrosoftOidc")
 builder.Services.ConfigureOidcRefreshHandling("Cookies", "MicrosoftOidc");
 builder.Services.AddAuthorization();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingAuthenticationStateProvider>();
-builder.Services.AddHttpForwarderWithServiceDiscovery();
 
 builder.AddAzureBlobClient("blob-docing");
 builder.AddRedisClient("redis");
 
 var redisConnection = ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("redis"));
 builder.Services.AddDataProtection()
-    .PersistKeysToStackExchangeRedis(redisConnection, "DataProtection-Keys")
-    ;
+    .PersistKeysToStackExchangeRedis(redisConnection, "DataProtection-Keys");
 
 builder.Services.AddScoped<AzureFileHelper>();
 
