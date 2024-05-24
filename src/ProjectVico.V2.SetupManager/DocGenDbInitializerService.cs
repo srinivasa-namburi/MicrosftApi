4d6776355706a997ecb9877dec46ff9d8383a287
Henning Kilset <hekilset@microsoft.com>
2024-05-24 08:27:29 +0200

    Executing Chat Conversation and history cleanup on startup. Should be moved to a scheduled job (which we currently don't have)

--- src/ProjectVico.V2.SetupManager/DocGenDbInitializerService.cs
+++ src/ProjectVico.V2.SetupManager/DocGenDbInitializerService.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Humanizer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using ProjectVico.V2.Shared.Configuration;
@@ -67,6 +68,8 @@ public class DocGenDbInitializerService : BackgroundService
         var sw = Stopwatch.StartNew();
 
         await Seed2024_04_07_IngestedDocumentDocumentProcess(dbContext, cancellationToken);
+        await Seed2024_05_24_OrphanedChatMessagesCleanup(dbContext, cancellationToken);
+        await Seed2024_05_24_ChatConversationsWithNoMessagesCleanup(dbContext, cancellationToken);
         
         sw.Stop();
         _logger.LogInformation("Seeding Document Generation Database completed in {ElapsedMilliseconds}ms", sw.ElapsedMilliseconds);
@@ -97,5 +100,72 @@ public class DocGenDbInitializerService : BackgroundService
             "US.NuclearLicensing");
 
         await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    private async Task Seed2024_05_24_OrphanedChatMessagesCleanup(DocGenerationDbContext dbContext,
+        CancellationToken cancellationToken)
+    {
+        // Remove any ChatMessages that are not associated with a ChatConversation
+        // First, get a count of the number of ChatMessages that are not associated with a ChatConversation. If it's 0, we don't need to do anything.
+
+        // The ChatMessages currently have a ConversationId in the model that is not nullable, so we can't have a null ConversationId.
+        // Therefore, we need to execute a raw SQL query to find ChatMessages where the ConversationId is null
+
+        var count = await dbContext.ChatMessages
+            .FromSqlRaw("SELECT * FROM ChatMessages WHERE ConversationId IS NULL")
+            .CountAsync(cancellationToken);
+        
+        if (count == 0)
+        {
+            _logger.LogInformation("No orphaned ChatMessages found. Skipping cleanup logic.");
+            return;
+        }
+
+        _logger.LogInformation("Cleaning up : Removing {Count} orphaned ChatMessages", count);
+
+        await dbContext.Database.ExecuteSqlRawAsync(
+            "DELETE FROM ChatMessages WHERE ConversationId IS NULL",
+            Guid.Empty);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    private async Task Seed2024_05_24_ChatConversationsWithNoMessagesCleanup(DocGenerationDbContext dbContext,
+        CancellationToken cancellationToken)
+    {
+        // Remove any ChatConversations that have no associated ChatMessages
+        // First, get a count of the number of ChatConversations that have no associated ChatMessages. If it's 0, we don't need to do anything.
+
+        // The ChatConversations currently have a list of ChatMessages in the model,
+        // so we can use LINQ to find ChatConversations with no associated ChatMessages
+
+        var expirePoint = DateTime.UtcNow-(7.Days());
+
+        var count = await dbContext.ChatConversations
+            .Where(x => x.ChatMessages.Count == 0)
+            .Where(x=>x.CreatedAt < expirePoint)
+            .CountAsync(cancellationToken);
+
+        if (count == 0)
+        {
+            _logger.LogInformation("No (old) ChatConversations found with no associated ChatMessages. Skipping cleanup logic.");
+            return;
+        }
+
+        _logger.LogInformation("Cleaning up : Removing {Count} ChatConversations with no associated ChatMessages that are older than 7 days", count);
+
+        var chatConversations = await dbContext.ChatConversations
+            .Where(x => x.ChatMessages.Count == 0)
+            .Where(x=>x.CreatedAt < expirePoint)
+            .ToListAsync(cancellationToken);
+
+        // This marks the ChatConversations for deletion through their IsActive property
+        dbContext.ChatConversations.RemoveRange(chatConversations);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        // This actually deletes the ChatConversations from the database
+        await dbContext.Database.ExecuteSqlRawAsync(
+            "DELETE FROM ChatConversations WHERE IsActive = 0", cancellationToken: cancellationToken);
+
     }
 }
